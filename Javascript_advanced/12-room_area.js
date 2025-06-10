59043402ab1df6e9862df0dac75d30b52c68676e
MOUKIANA-jordy <jordymoukiana@gmail.com>
2025-06-10 16:29:35 +0200

    add room

---
+++ Javascript_advanced/12-room_area.js
@@ -0,0 +1,11 @@
+// 12-room_area.js
+
+const roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea() {
+    return this.width * this.length;
+  }
+};
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
