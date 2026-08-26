5e88b280f2ce4a490e912189db3c70dbff663695
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-08-26 14:24:10 +0200

    Change prime calculation stack order

--- Javascript_advanced/11-prime.js
+++ Javascript_advanced/11-prime.js
@@ -1,34 +1,38 @@
-// 11-prime.js
-
-function isPrime(num) {
-  if (num < 2) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) return false;
-  }
-  return true;
-}
-
 function countPrimeNumbers() {
   let count = 0;
-  for (let i = 2; i <= 100; i++) {
-    if (isPrime(i)) {
+
+  for (let number = 2; number <= 100; number++) {
+    let isPrime = true;
+
+    for (
+      let divisor = 2;
+      divisor < number;
+      divisor++
+    ) {
+      if (number % divisor === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) {
       count++;
     }
   }
+
   return count;
 }
 
-const { performance } = window;
+const startTime = performance.now();
 
-console.log('Starting timer...');
+for (let i = 0; i < 100; i++) {
+  setTimeout(countPrimeNumbers, 0);
+}
 
-const start = performance.now();
+const endTime = performance.now();
 
-setTimeout(() => {
-  for (let i = 0; i < 100; i++) {
-    countPrimeNumbers();
-  }
-  const end = performance.now();
-  console.log(`Execution time of calculating prime numbers 100 times was ${end - start} milliseconds.`);
-}, 0);
-
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${
+    endTime - startTime
+  } milliseconds.`
+);
